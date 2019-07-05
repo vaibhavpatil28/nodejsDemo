4d90a6fbd82d97036e0d51b1069519d7bf1bd23e
Vaibhav Patil <melayer.vaibhav@gmail.com>
2019-07-05 10:47:30 +0530

    connection to  ms sql server is done successfully

--- index.js
+++ index.js
@@ -2,7 +2,35 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  var sql = require("mssql");
+
+  // config for your database
+  var config = {
+      user: 'sa',
+      password: '@Angular8;',
+      server: 'localhost', 
+      database: 'TestData' 
+  };
+
+  // connect to your database
+  sql.connect(config, function (err) {
+  
+      if (err) console.log(err);
+
+      // create Request object
+      var request = new sql.Request();
+         
+      // query to the database and get the records
+      request.query(`SELECT ProductID, ProductName, Price, ProductDescription  
+      FROM dbo.Products`, function (err, recordset) {
+          
+          if (err) console.log(err)
+
+          // send records as a response
+          res.send(recordset);
+          
+      });
+  });
 });
 
 app.listen(8000, () => {
